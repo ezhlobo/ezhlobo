7b7f3ed01bac56666ee7f1c6446dbdf153ba617e
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2015-02-28 14:59:30 +0300

    Remove artifact: console.log

--- _includes/disqus.js
+++ _includes/disqus.js
@@ -4,7 +4,6 @@
         dsq.type = 'text/javascript';
         dsq.async = true;
         dsq.src = '//go.disqus.com/embed.js';
-        console.log('as');
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
 })();
